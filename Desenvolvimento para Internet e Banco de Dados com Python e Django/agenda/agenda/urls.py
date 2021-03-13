322b9b457cb8a8259a2bc466cbfcde7727eea289
Renan Torres <rgrtorres@hotmail.com>
2021-03-13 10:22:46 -0300

    Aprendendo banco de dados django

--- Desenvolvimento para Internet e Banco de Dados com Python e Django/agenda/agenda/urls.py
+++ Desenvolvimento para Internet e Banco de Dados com Python e Django/agenda/agenda/urls.py
@@ -1,4 +1,4 @@
-"""hello_django URL Configuration
+"""agenda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/<int:idade>/', views.hello)
 ]
